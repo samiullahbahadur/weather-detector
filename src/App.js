990993604e3e90ed725b16c0ee536a920d335abb
samiullahbahadur <bahadursamiullah@gmail.com>
2022-06-05 01:14:59 +0430

    Refactor code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import React from "react";
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: null,
+      errorMessage: "",
+    };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          lat: position.coords.latitude,
+        });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude:{this.state.lat}</div>;
+    }
+    return <div>loading...!</div>;
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import App from './App';
+import SeasonDisplay from './seasonDisplay';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root =
+   ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <SeasonDisplay />
   </React.StrictMode>
+  
 );
 
 
